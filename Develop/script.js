55ec4f537c3c6b740daffa5bc159e6b01bbd50c0
bg <62928419+bgeertsen@users.noreply.github.com>
2022-05-20 19:21:41 -0700

    made basic password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,42 @@
 // Assignment code here
+function getRandomInt (max) {
+  return Math.floor(Math.random() * max);
+}
 
+var passwordChars = {
+  lower: "abcdefghijklmnopqrstuvxyz",
+  upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  special: "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
+  numeric: "0123456789"
+}
+
+function confirmLength() {
+  passwordLength = 0;
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = window.prompt("Pick a password legnth between 8 and 128 characters long.")
+  }
+}
+
+function generatePassword() {
+  var phrase = ""
+  confirmLength();
+  var confirmUpper = window.confirm("Does the password require upper case characters in the password?")
+  var confirmSpecial = window.confirm("Does the password require special characters?")
+  var confirmNumeric = window.confirm("Does the password require numeric characters?")
+  if (confirmUpper) {
+    passwordChars.lower += passwordChars.upper;
+  }
+  if (confirmSpecial) {
+    passwordChars.lower += passwordChars.special;
+  }
+  if (confirmNumeric) {
+    passwordChars.lower += passwordChars.numeric;
+  }
+  for(var i = 0; i < passwordLength; i ++) {
+    phrase += passwordChars.lower[getRandomInt(passwordChars.lower.length)];
+  }
+  return phrase;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +47,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
